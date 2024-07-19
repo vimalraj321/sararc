ecf67cf3874cf8f876bf05db5c05cc0b95ee47b9
Vimal raj R <vimal@Vimals-MacBook-Pro.local>
2024-07-19 22:02:59 +0530

    test

--- sections/Team.tsx
+++ sections/Team.tsx
@@ -1,21 +1,20 @@
-import { Card } from '@/components/Card'
-import { Container } from '@/components/Container'
-import { team } from "@/constant/data.json"
+import { Card } from "@/components/Card";
+import { Container } from "@/components/Container";
+import { team } from "@/constant/data.json";
 
 export const Team = () => {
   return (
     <Container>
-      <div className=''>
-        <h2 className='text-3xl font-bold text-center'>{team.heading}</h2>
-        <div className='flex flex-col gap-[60px] md:gap-[150px] mt-[50px]'>
-          {
-            team.cards.map((teammember, i) => (
-              <Card key={i}   {...teammember} />
-            ))
-          }
+      <div className="">
+        <h2 className="text-3xl font-bold text-center">{team.heading}</h2>
+        <div className="flex flex-col gap-[60px] md:gap-[150px] mt-[50px]">
+          {team.cards.map((teammember, i) => (
+            <Card key={i} {...teammember} />
+          ))}
         </div>
-      </div >
+      </div>
     </Container>
-  )
-}
+  );
+};
 
+// checking
